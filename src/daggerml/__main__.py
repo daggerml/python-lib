132e62d031d78063cba14f80f49457fe5a0076f7
Micha Niskin <micha.niskin@gmail.com>
2023-01-05 00:36:27 -0500

    wip

--- src/daggerml/__main__.py
+++ src/daggerml/__main__.py
@@ -1,10 +1,8 @@
 import click
-import os
 import sys
 import daggerml as dml
 import daggerml._config as config
 import logging
-from daggerml._config import DML_PROFILE, DML_GROUP_ID, DML_API_ENDPOINT
 
 
 logger = logging.getLogger(__name__)
@@ -17,16 +15,15 @@ def cli():
 
 @cli.command('configure', context_settings={'show_default': True}, help='configure DaggerML API')
 @click.option('--global', is_flag=True, help='update global configuration')
-@click.option('--profile', default=DML_PROFILE or 'DEFAULT', help='configuration profile')
+@click.option('--profile', default='DEFAULT', help='configuration profile')
 @click.option('--group-id', default=None, help='group ID')
 @click.option('--api-endpoint', default=None, help='API endpoint')
 def cli_configure(profile, group_id, api_endpoint, **kwargs):
-    print('got:', profile, group_id, api_endpoint, kwargs)
     config.update_config(profile, group_id, api_endpoint, kwargs['global'])
 
 
 @cli.command('login', context_settings={'show_default': True}, help='create DaggerML API key')
-@click.option('--profile', default=DML_PROFILE or 'DEFAULT', help='configuration profile')
+@click.option('--profile', default='DEFAULT', help='configuration profile')
 @click.option('--username', required=True, help='user name')
 @click.option('--password', required=True, prompt=True, hide_input=True, help='read from stdin if not specified')
 def cli_login(profile, username, password):
