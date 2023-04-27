564c326d05631bc17d1d9252d44383812603215a
Micha Niskin <micha.niskin@gmail.com>
2023-04-27 01:40:48 -0400

    wip

--- src/daggerml/__init__.py
+++ src/daggerml/__init__.py
@@ -1,6 +1,6 @@
 from daggerml._dag import (  # noqa: F401
     DmlError, DagError, ApiError, NodeError, Resource,
-    list_dags, describe_dag, get_dag_by_name_version,
+    list_dags, describe_dag, delete_dag, get_dag_by_name_version,
     Dag, Node, register_tag
 )
 from pkg_resources import get_distribution, DistributionNotFound
