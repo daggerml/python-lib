9a51c0e3960f2c1fcf3a38e24c3a86082e63a104
Aaron Niskin <amniskin@gmail.com>
2024-03-17 10:21:48 -0700

    wip

--- src/daggerml/__init__.py
+++ src/daggerml/__init__.py
@@ -1,10 +1,12 @@
 from importlib.metadata import PackageNotFoundError, version
 
-import daggerml.util as _util
-from daggerml.api import Dag, Node, dump_obj, load_obj
-from daggerml.api import load_state_file as load
-from daggerml.core import Datum, Error, Ref, Resource, Scalar
-from daggerml.util import ApiError, from_data, from_json, to_data, to_json
+from daggerml.core import Dag, Error, Node, Resource, _api, from_data, from_json, to_data, to_json
+
+# import daggerml.util as _util
+# from daggerml.api import Dag, Node, dump_obj, load_obj
+# from daggerml.api import load_state_file as load
+# from daggerml.core import Datum, Error, Ref, Resource, Scalar
+# from daggerml.util import ApiError, from_data, from_json, to_data, to_json
 
 try:
     __version__ = version("daggerml")
@@ -13,13 +15,9 @@ except PackageNotFoundError:
 
 del version, PackageNotFoundError
 
-def set_flags(kw):
-    _util.CLI_FLAGS = _util.CLI_FLAGS.update(kw)
-
-def revert_flags():
-    _util.CLI_FLAGS = _util.CLI_FLAGS.revert()
-
+def new(name, message, flags=None):
+    return Dag.new(name, message, api_flags=flags)
 
 __all__ = (
-    'Dag', 'Datum', 'Error', 'Node', 'Ref', 'Resource', 'Scalar', 'ApiError'
+    'Dag', 'Error', 'Node', 'Resource', 'from_data', 'to_data', 'from_json', 'to_json', 'new'
 )
--- src/daggerml/core.py
+++ src/daggerml/core.py
@@ -1,159 +1,213 @@
+#!/usr/bin/env python3
+import json
 import logging
-from dataclasses import dataclass
-
-from daggerml.util import Error, Resource, dml_type, invoke_api
+import subprocess
+import traceback as tb
+from dataclasses import dataclass, field, fields
+from typing import Any, Dict, List
 
 logger = logging.getLogger(__name__)
 
+DATA_TYPE = {}
 
-@dml_type
-@dataclass
-class Ref:
-    to: str | None
 
-    @property
-    def type(self):
-        return self.to.split('/', 1)[0] if self.to else None
-
-    @property
-    def name(self):
-        return self.to.split('/', 1)[1] if self.to else None
-
-    def __call__(self):
-        # FIXME: maybe we have a "deref" endpoint instead?
-        return invoke_api(None, 'get_ref', self)
-
-    def __hash__(self):
-        return hash(self.to)
+def dml_type(cls=None):
+    def decorator(cls):
+        DATA_TYPE[cls.__name__] = cls
+        return cls
+    return decorator(cls) if cls else decorator
 
 
 @dml_type
-@dataclass
-class Repo:
-    path: str
-    user: str
-    head: Ref
-    index: Ref | None
-    dag: Ref | None
-    parent_dag: Ref | None
-    cached_dag: Ref | None
-
-    def _invoke(self, op, *args, **kwargs):
-        resp = invoke_api(self, op, *args, **kwargs)
-        # if isinstance(resp, Node):
-        #     self.nodes.add(resp)
-        return resp
-
-    def begin(self, expr) -> "Repo":
-        repo = self._invoke('begin', expr=expr)
-        assert isinstance(repo, Repo)
-        return repo
-
-    def put_literal(self, data) -> Ref:
-        return self._invoke('put_literal', data)
-
-    def put_load(self, dag_name) -> Ref:
-        return self._invoke('put_load', dag_name)
-
-    def commit(self, result, cache=None) -> Ref|None:
-        return self._invoke('commit', result, cache)
-
-    def deref(self, ref):
-        raise Exception('get_ref')
-
-
-def create_dag(name: str, message: str|None = None) -> Repo:
-    repo = invoke_api(None, 'begin', name=name, message=message)
-    assert isinstance(repo, Repo)  # make my fucking linter happy
-    return repo
-
-
-@dml_type
-@dataclass
-class Dag:
-    nodes: set[Ref]  # -> node
-    result: Ref | None  # -> node
-    error: Error | None
-
-
-@dml_type
-@dataclass
-class FnDag(Dag):
-    expr: list[Ref]  # -> node
-
-
-@dml_type
-@dataclass
-class CachedFnDag(Dag):
-    expr: list[Ref]  # -> node
-
+@dataclass  # (frozen=True)
+class Resource:
+    data: Dict[str,str]
 
 Scalar = str | int | float | bool | type(None) | Resource
 
-
 @dml_type
 @dataclass
-class Datum:
-    value: list | dict | set | Scalar
+class Error(Exception):
+    message: str
+    context: dict = field(default_factory=dict)
+    code: str|None = None
 
-    def unroll(self):
-        val = self.value
-        if isinstance(val, Scalar):
-            return val
-        if isinstance(val, list|tuple):
-            return [x().unroll() for x in val]
-        if isinstance(val, dict):
-            return {k: v().unroll() for k, v in val.items()}
-        if isinstance(val, set):
-            return {v().unroll() for v in val}
-        raise ValueError('unrecognized type: %r' % type(val))
+    def __post_init__(self):
+        self.code = type(self).__name__ if self.code is None else self.code
+
+    @classmethod
+    def from_ex(cls, ex):
+        if isinstance(ex, Error):
+            return ex
+        formatted_tb = tb.format_exception(type(ex), value=ex, tb=ex.__traceback__)
+        return cls(str(ex), {'trace': formatted_tb}, type(ex).__name__)
 
 
 @dml_type
-@dataclass
-class Literal:
-    value: Ref  # -> datum
+@dataclass(frozen=True)
+class Ref:
+    to: str
 
     @property
-    def error(self):
-        pass
+    def type(self):
+        return self.to.split('/')[0]
 
 
-@dml_type
-@dataclass
-class Load:
-    dag: Ref
-
-    @property
-    def value(self):
-        dag = self.dag()
-        assert isinstance(dag, Dag)
-        if dag.result is None:
-            if dag.error is None:
-                raise RuntimeError('cannot get dag value for unfinished dag')
-            raise dag.error
-        return dag.result().value
-
-    @property
-    def error(self) -> Error:
-        return self.dag().error
+def from_data(data):
+    n, *args = data if isinstance(data, list) else [None, data]
+    if n is None:
+        return args[0]
+    if n == 'l':
+        return [from_data(x) for x in args]
+    if n == 's':
+        return {from_data(x) for x in args}
+    if n == 'd':
+        return {k: from_data(v) for (k, v) in args}
+    if n in DATA_TYPE:
+        return DATA_TYPE[n](*[from_data(x) for x in args])
+    raise ValueError(f'cannot `from_data` {data!r}')
+    # return Ref(data)
 
 
-@dml_type
-@dataclass
-class Fn(Load):
-    expr: list[Ref]  # -> node
+def to_data(obj):
+    if isinstance(obj, tuple):
+        obj = list(obj)
+    n = obj.__class__.__name__
+    if isinstance(obj, (type(None), str, bool, int, float)):
+        return obj
+    if isinstance(obj, (list, set)):
+        return [n[0], *[to_data(x) for x in obj]]
+    if isinstance(obj, dict):
+        return [n[0], *[[k, to_data(v)] for k, v in obj.items()]]
+    if n in DATA_TYPE:
+        return [n, *[to_data(getattr(obj, x.name)) for x in fields(obj)]]
+    raise ValueError(f'no data encoding for type: {n}')
 
 
-@dml_type
-@dataclass
+def from_json(text):
+    return from_data(json.loads(text))
+
+
+def to_json(obj):
+    return json.dumps(to_data(obj), separators=(',', ':'))
+
+
+class ApiError(Error):
+    pass
+
+
+def _api(*args):
+    try:
+        cmd = ['dml', *args]
+        logger.debug('cmd args: %r', cmd)
+        resp = subprocess.run(cmd, capture_output=True)
+        logger.debug('response: %r', resp.stdout)
+        return resp.stdout.decode()
+    except KeyboardInterrupt:
+        raise
+    except Exception as e:
+        raise Error.from_ex(e) from e
+
+
+def invoke_api(token, op, *args, **kwargs):
+    payload = to_json([op, args, kwargs])
+    resp = _api('dag', 'invoke', token, payload)
+    data = from_json(resp)
+    if isinstance(data, Error):
+        raise data
+    return data
+
+@dataclass(frozen=True)
 class Node:
-    data: Literal | Load | Fn
+    ref: Ref
+
+@dataclass(frozen=True)
+class Dag:
+    token: str
+    api_flags: Dict[str, str] = field(default_factory=dict)
+
+    @classmethod
+    def new(cls, name: str, message: str, api_flags: Dict[str, str]|None = None) -> "Dag":
+        api_flags = api_flags or {}
+        tok = _api(*cls._to_flags(api_flags), 'dag', 'create', name, message)
+        return cls(tok, api_flags=api_flags.copy())
+
+    @staticmethod
+    def _to_flags(flag_dict: Dict[str, str]) -> List[str]:
+        out = []
+        for k, v in sorted(flag_dict.items()):
+            out.extend([f'--{k}', v])
+        return out
+
+    def _api(self, *args):
+        return _api(*self._to_flags(self.api_flags), *args)
+
+    def _invoke(self, op, *args, **kwargs):
+        payload = to_json([op, args, kwargs])
+        resp = self._api('dag', 'invoke', self.token, payload)
+        return resp
+
+    def invoke(self, op, *args, **kwargs):
+        resp = self._invoke(op, *args, **kwargs)
+        data = from_json(resp)
+        if isinstance(data, Error):
+            raise data
+        return data
+
+    def start_fn(self, *expr, cache: bool = True, retry: bool = False) -> "Dag|Node":
+        expr = [x if isinstance(x, Node) else self.put(x) for x in expr]
+        expr = [x.ref for x in expr]
+        token = self._invoke('start_fn', expr=expr, cache=cache, retry=retry)
+        ref = from_json(token)
+        assert isinstance(ref, Ref)
+        if ref.type == 'node':
+            return Node(ref)
+        return Dag(token, self.api_flags.copy())
+
+    def call(self, f, *args, cache: bool = True, resource: Resource|None = None) -> "Dag|Node":
+        resource = resource or Resource({'exec': 'local'})
+        fndag = self.start_fn(self.put(resource), *args, cache=cache)
+        if isinstance(fndag, Node):
+            return fndag
+        with fndag:
+            result = f(*fndag.expr[1:])
+            result = fndag.put(result)
+            node = fndag.commit(result, cache=cache)
+            assert isinstance(node, Node)
+            return node
 
     @property
-    def value(self):
-        return self.data.value
+    def expr(self):
+        return self.invoke('get_expr')
 
-    @property
-    def error(self) -> Error:
-        return self.data.error
+    def put(self, data) -> Node:
+        resp = self.invoke('put_literal', data)
+        assert isinstance(resp, Ref)
+        return Node(resp)
+
+    def load(self, dag_name) -> Node:
+        resp = self.invoke('put_load', dag_name)
+        assert isinstance(resp, Ref)
+        return Node(resp)
+
+    def commit(self, result, cache: bool = True) -> Node|None:
+        if isinstance(result, Node):
+            result = result.ref
+        resp = self.invoke('commit', result, cache=cache)
+        if resp is None:
+            return
+        assert isinstance(resp, Ref)
+        return Node(resp)
+
+    def get_value(self, node: Node) -> Any:
+        val = self.invoke('get_node_value', node.ref)
+        return val
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, err_type, exc_val, err_tb):
+        if exc_val is not None:
+            ex = Error.from_ex(exc_val)
+            logger.exception('failing dag with error code: %r', ex.code)
+            self.commit(ex)
