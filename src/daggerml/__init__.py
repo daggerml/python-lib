c70d7997ea0a8b8f63c700d5b6dbf5ce79cfcd7d
Aaron Niskin <amniskin@gmail.com>
2025-01-22 23:55:19 -0800

    fixed a bug in Error repr and added docs

--- src/daggerml/__init__.py
+++ src/daggerml/__init__.py
@@ -1,3 +1,9 @@
+"""
+DaggerML - A Python library for building and managing directed acyclic graphs.
+
+This library provides tools for creating, manipulating, and executing DAGs
+with strong typing support and a context-manager based interface.
+"""
 from importlib.metadata import PackageNotFoundError, version
 
 from daggerml.core import Dag, Dml, Error, Node, Ref, Resource, from_json, to_json
@@ -11,6 +17,21 @@ del version, PackageNotFoundError
 
 
 def new(name, message):
+    """
+    Create a new DAG with the given name and message.
+
+    Parameters
+    ----------
+    name : str
+        Name of the DAG
+    message : str
+        Commit message or description
+
+    Returns
+    -------
+    Dag
+        A new DAG instance
+    """
     return Dml().new(name, message)
 
 
