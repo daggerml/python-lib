b17e23dccbfc490588cfc15a8f11c2d46a64004e
Micha Niskin <micha.niskin@gmail.com>
2025-01-31 05:16:49 -0500

    nothing to see here

--- src/daggerml/__init__.py
+++ src/daggerml/__init__.py
@@ -5,7 +5,7 @@ This library provides tools for creating, manipulating, and executing DAGs
 with strong typing support and a context-manager based interface.
 """
 
-from daggerml.core import Dml, Resource
+from daggerml.core import Dml, Error, Node, Resource
 
 try:
     from daggerml.__about__ import __version__
@@ -13,23 +13,4 @@ except ImportError:
     __version__ = "local"
 
 
-def new(name, message, **kwargs):
-    """
-    Create a new DAG with the given name and message.
-
-    Parameters
-    ----------
-    name : str
-        Name of the DAG
-    message : str
-        Commit message or description
-
-    Returns
-    -------
-    Dag
-        A new DAG instance
-    """
-    return Dml(**kwargs).new(name, message)
-
-
-__all__ = ("Dml", "Resource")
+__all__ = ("Dml", "Error", "Node", "Resource")
